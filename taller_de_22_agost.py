a175becedc6b2257929b2364a44360f89dfd097f
nicksasan <nssanchez159@soy.sena.edu.co>
2024-08-22 13:13:56 -0500

    hola

--- taller_de_22_agost.py
+++ taller_de_22_agost.py
@@ -1,7 +1,30 @@
 #Menu para escoger suma, resta y multiplicacion
-print("1. Suma \n2. Resta \n3. Multiplicacion")
+print("1. Suma \n2. Resta \n3. Multiplicacion \n4. Salir")
+opcion = input("Ingresa un numero de opcion: ")
+
+def suma(a,b):
+    return a + b 
+
+def resta(a,b):
+    return a - b
+
+def multiplicacion(a,b):
+    return a * b
 
 a = int(input("Escribe el primer numero: "))
 b = int(input("Escribe el segundo numero: "))
-c = a + b
-print("la suma es: ", c)
+
+if opcion ==1:
+    c = a + b
+    print("La suma es: ", c)
+    
+elif opcion ==2: 
+    c = a - b
+    print("La resta es: ", c)
+    
+elif opcion ==3:
+    c = a * b
+    print("La multiplicacion es: ", c)
+    
+elif opcion ==4:
+    print("baybay kaka")
