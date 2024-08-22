d89973f9a22a13fda0bae5218f0d46c7e8e7776f
nicksasan <nssanchez159@soy.sena.edu.co>
2024-08-22 13:12:31 -0500

    modificaciones

--- taller_de_22_agost.py
+++ taller_de_22_agost.py
@@ -1,4 +1,5 @@
 #Menu para escoger suma, resta y multiplicacion
+print("1. Suma \n2. Resta \n3. Multiplicacion")
 
 a = int(input("Escribe el primer numero: "))
 b = int(input("Escribe el segundo numero: "))
