a4fb31490300191020c6d46dec04433e9f4b868f
Nixson Roriguez <nixsonfabian8@gmail.com>
2024-08-22 12:54:21 -0500

    primera modificacion

---
+++ taller_de_22_agost.py
@@ -0,0 +1,5 @@
+# Codigo para sumas dos variables.
+numero1 = int(input("Digite el primer numero: "))
+numero2 = int(input("Digite el segundo numero: "))
+r = (numero1 + numero2)
+print (f"La suma de los dos numeros des de {r}.")
