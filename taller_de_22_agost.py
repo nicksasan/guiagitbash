a88492e21a23edd6530a69d6434e5aed9778f3d1
nicksasan <nssanchez159@soy.sena.edu.co>
2024-08-23 11:00:20 -0500

    Modificacion con el codigo bien

--- taller_de_22_agost.py
+++ taller_de_22_agost.py
@@ -1,6 +1,6 @@
 #Menu para escoger suma, resta y multiplicacion
 print("1. Suma \n2. Resta \n3. Multiplicacion \n4. Salir")
-opcion = input("Ingresa un numero de opcion: ")
+opcion = int(input("Ingresa un numero de opcion: "))
 
 def suma(a,b):
     return a + b 
@@ -11,20 +11,24 @@ def resta(a,b):
 def multiplicacion(a,b):
     return a * b
 
-a = int(input("Escribe el primer numero: "))
-b = int(input("Escribe el segundo numero: "))
-
 if opcion ==1:
-    c = a + b
+    a = int(input("Escribe el primer numero: "))
+    b = int(input("Escribe el segundo numero: "))
+    c = suma(a, b)
     print("La suma es: ", c)
     
 elif opcion ==2: 
-    c = a - b
+    a = int(input("Escribe el primer numero: "))
+    b = int(input("Escribe el segundo numero: "))
+    c = resta(a, b)
     print("La resta es: ", c)
     
 elif opcion ==3:
+    a = int(input("Escribe el primer numero: "))
+    b = int(input("Escribe el segundo numero: "))
+    c = multiplicacion(a, b)
     c = a * b
-    print("La multiplicacion es xd: ", c)
+    print("La multiplicacion es: ", c)
     
 elif opcion ==4:
     print("baybay uwu")
