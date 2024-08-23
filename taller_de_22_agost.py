0cee03090ef342076a6c5437bb44d7d5f561652e
Nixson Roriguez <nixsonfabian8@gmail.com>
2024-08-23 10:52:08 -0500

    segunda modificacion

--- taller_de_22_agost.py
+++ taller_de_22_agost.py
@@ -3,3 +3,9 @@ numero1 = int(input("Digite el primer numero: "))
 numero2 = int(input("Digite el segundo numero: "))
 r = (numero1 + numero2)
 print (f"La suma de los dos numeros des de {r}.")
+
+# Codigo de multiplicar dos numeros.
+numero3 = int(input("Digite el primer numero: "))
+numero4 = int(input("Digite el segundo numero: "))
+multiplicar = (numero3 * numero4)
+print (f"La multiplicacion de los dos numeros es de {multiplicar}.")
