e0b32c21990307f64360a93ce8b958a1294fb695
Nixson Roriguez <nixsonfabian8@gmail.com>
2024-08-23 10:53:42 -0500

    Tercera modificacion

--- taller_de_22_agost.py
+++ taller_de_22_agost.py
@@ -9,3 +9,9 @@ numero3 = int(input("Digite el primer numero: "))
 numero4 = int(input("Digite el segundo numero: "))
 multiplicar = (numero3 * numero4)
 print (f"La multiplicacion de los dos numeros es de {multiplicar}.")
+
+# Codigo para dividir dos numeros .
+numero5 = int(input("Digite el primer numero: "))
+numero6 = int(input("Digite el segundo numero: "))
+divicion = (numero5 / numero6)
+print (f"La division de los dos numeros es de {divicion}.")
